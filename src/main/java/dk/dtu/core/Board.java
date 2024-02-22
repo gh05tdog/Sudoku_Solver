0fbc00d10d63130e1439afecbbaf83fa9dbe52f1
Oliver Hansen <s224754@dtu.dk>
2024-02-22 14:51:46 +0100

    Create Board Class

--- src/main/java/dk/dtu/core/Board.java
+++ src/main/java/dk/dtu/core/Board.java
@@ -1,37 +1,88 @@
 package dk.dtu.core;
 
+import java.util.ArrayList;
+
 public class Board {
 
-    private int n;
-    private int k;
+    private final int n;
+    private final int k;
 
-    private int[][] board;
+    private ArrayList<ArrayList<Integer>> board;
 
-    public Board(int k, int n) throws Exception {
+    public Board(int n, int k) throws Exception {
         this.n = n;
         this.k = k;
-        // Create empty board array
-        this.board = createEmptyBoard(n, k);
-    }
 
-    public int[][] createEmptyBoard(int k, int n) throws Exception {
-        //Check if board is possible to create
-
-        if (!boardIsPossible(k, n)) {
+        if (boardIsPossible(k, n)) {
             throw new Exception("This board is not possible to create");
         }
-        int num = n * n;
-        return new int[num][num];
+        this.board = new ArrayList<>();
+        int numRows = n * n;
+        // Initialize the board with zeros
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> row = new ArrayList<>(numRows);
+            for (int j = 0; j < numRows; j++) {
+                row.add(0);
+            }
+            board.add(row);
+        }
     }
 
-    public int[][] getBoard(){
+    public ArrayList<ArrayList<Integer>> getBoard() {
         return this.board;
     }
 
-    public void setBoard (int[][] board){
+    public void setBoard(ArrayList<ArrayList<Integer>> board) {
         this.board = board;
     }
+
+    public int totalNoOfSquares() {
+        return (int) (Math.pow(k, 2) * (n * n));
+    }
+
     private boolean boardIsPossible(int k, int n) {
-        return Math.pow(k, 2) == n * n;
+        return Math.pow(k, 2) != n * n;
+    }
+
+    public void setNumber(int x, int y, int num) {
+        this.board.get(x).set(y, num);
+    }
+
+    public boolean validPlace(int x, int y, int num) {
+
+        // Check rows
+        if( getRow(x).contains(num) || !getColumn(y).contains(num)){
+            return false;
+        }else{
+            //Check square
+            return !getSquare(x, y).contains(num);
+        }
+    }
+
+    private ArrayList<Integer> getRow(int i) {
+        return this.board.get(i);
+    }
+
+    private ArrayList<Integer> getColumn(int i) {
+        ArrayList<Integer> column = new ArrayList<>(this.board.size()); //
+        for (ArrayList<Integer> row : board) {
+            column.add(row.get(i));
+        }
+        return column;
+    }
+
+    private ArrayList<Integer> getSquare(int x, int y) {
+        ArrayList<Integer> square = new ArrayList<>();
+        int squareRowStart = (x / n) * n;
+        int squareColStart = (y / n) * n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int currentX = squareRowStart + i;
+                int currentY = squareColStart + j;
+                square.add(this.board.get(currentX).get(currentY));
+            }
+        }
+        return square;
     }
 }
