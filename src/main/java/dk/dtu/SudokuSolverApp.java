c5db89b81e426ea128953a177b5715dd898118fd
Martin-Surlykke <116875874+Martin-Surlykke@users.noreply.github.com>
2024-04-01 17:02:08 +0200

    less numbers removed to check functionality

--- src/main/java/dk/dtu/SudokuSolverApp.java
+++ src/main/java/dk/dtu/SudokuSolverApp.java
@@ -7,8 +7,8 @@ class SudokuSolverApp {
     public static void main(String[] args) throws Exception {
         // Set up the window manager and game parameters.
         WindowManager windowManager = new WindowManager(900, 900);
-        int n = 4;
-        int k = 4;
+        int n = 3;
+        int k = 3;
         int cellSize = 550/(n*k);
         // Initialize the GameEngine with the window manager.
         GameEngine gameEngine = new GameEngine(windowManager, n, k, cellSize);
