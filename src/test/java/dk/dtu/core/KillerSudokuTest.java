383567ce12a4626eecdc7e6d1f4e57dde1e44a56
Oliver Hansen <s224754@dtu.dk>
2024-06-13 12:46:35 +0200

    Fully working load and save with killersudoku

--- src/test/java/dk/dtu/core/KillerSudokuTest.java
+++ src/test/java/dk/dtu/core/KillerSudokuTest.java
@@ -10,16 +10,15 @@ import dk.dtu.engine.graphics.SudokuBoardCanvas;
 import dk.dtu.engine.utility.CustomBoardPanel;
 import dk.dtu.engine.utility.CustomComponentGroup;
 import dk.dtu.game.core.*;
-import java.awt.*;
+
 import javax.swing.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class KillerSudokuTest {
 
     private SudokuGame game;
-    private CustomComponentGroup componentGroup;
-    private CustomBoardPanel panel2;
     private SudokuBoardCanvas sudokuBoardCanvasBoard;
 
     @BeforeEach
@@ -37,11 +36,11 @@ public class KillerSudokuTest {
         game = new SudokuGame(windowManager, 3, 3, 550 / 9);
         game.initialize(3, 3, 550 / 9);
         windowManager.setHeart();
-        componentGroup = new CustomComponentGroup();
+        CustomComponentGroup componentGroup = new CustomComponentGroup();
 
         // Create mock panels
         CustomBoardPanel panel1 = new CustomBoardPanel();
-        panel2 = new CustomBoardPanel();
+        CustomBoardPanel panel2 = new CustomBoardPanel();
         CustomBoardPanel panel3 = new CustomBoardPanel();
 
         // Add panels to the group
@@ -63,28 +62,4 @@ public class KillerSudokuTest {
         assertTrue(sudokuBoardCanvasBoard.getCages().getFirst().getSum() > 0);
     }
 
-    @Test
-    void testAdjustInitialNumbersVisibility() {
-        Config.setDifficulty("easy");
-        game.adjustInitialNumbersVisibility();
-
-        long countVisibleNumbers;
-
-        Config.setDifficulty("extreme");
-        game.adjustInitialNumbersVisibility();
-        countVisibleNumbers = countVisibleNumbers();
-        assertEquals(0, countVisibleNumbers); // Check extreme difficulty, no numbers visible
-    }
-
-    private long countVisibleNumbers() {
-        int count = 0;
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (game.gameboard.getNumber(row, col) > 0) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
 }
